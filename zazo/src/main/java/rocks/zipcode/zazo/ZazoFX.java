26b0fcdf8409f92d3f2dfebb0626cc384020c9cf
Kristofer <kris@youngers.org>
2024-10-22 09:47:17 -0400

    imports

--- zazo/src/main/java/rocks/zipcode/zazo/ZazoFX.java
+++ zazo/src/main/java/rocks/zipcode/zazo/ZazoFX.java
@@ -13,7 +13,6 @@ import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.net.URI;
